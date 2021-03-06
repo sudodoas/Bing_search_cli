c426782fe20c5adba0f88ec078902ceaf8431709
sudo doas <doassudo@gmail.com>
2021-03-06 14:11:13 +0200

    finnally working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Bing_search_cli
+Search Bing Images from the cli
+Sorry, I broke the script but I think it is working now.
--- test.sh
+++ test.sh
@@ -2,25 +2,26 @@
 
 echo "Type in a search query:"
 
-#read QUERY
-#QUERY=$(cat ~/code/spam/beemovie.txt | dmenu -p "What images do you want to search for" -fn monospace:size=15 -nb "#282c34" -nf "#bbbbbb" -sb "#3071db" -sf "#282c34")
-#QUERY=$(cat ~/code/spam/beemovie.txt | fzf)
 read QUERY
 
+
 echo "#!/bin/sh" > ~/.local/tmp/bingsearch.sh
-echo "cd ~/.local/tmp/" >> ~/.local/tmp/bingsearch.sh
-echo "rm *=*" >> ~/.local/tmp/bingsearch.sh
+echo "cd ~/.local/tmp/bing_images" >> ~/.local/tmp/bingsearch.sh
+echo "rm *" >> ~/.local/tmp/bingsearch.sh
 
 
-curl "https://www.bing.com/images/search?q=$QUERY&form=QBLH&sp=-1&pq=$QUERY&sc=8-4&qs=n&cvid=374ABE0AFA67447AA129F7F8CA6FA78E&first=1&tsc=ImageBasicHover" |
+choice=$(curl "https://www.bing.com/images/search?q=$QUERY&form=HDRSC2&first=1&tsc=ImageBasicHover" |
     pup -c |
-    grep img --color |
-    grep -E  -o  \"'http.*1.1'\" --color >> ~/.local/tmp/bingsearch.sh
+    grep --color -E -o '"https://th.bing.*"' >> ~/.local/tmp/bingsearch.sh)
 
+echo $choice
 
-    sed -i 's/"http/wget "http/g' ~/.local/tmp/bingsearch.sh
-    sed -i 's/1"/1" \\/g' ~/.local/tmp/bingsearch.sh
+sed -i 's/"http/wget "http/g' ~/.local/tmp/bingsearch.sh
+
+#    sed -i 's/href="//g' ~/.local/tmp/bingsearch.sh
+#    sed -i 's/http/wget "http/g' ~/.local/tmp/bingsearch.sh
+#    sed -i 's/1"/1" \\/g' ~/.local/tmp/bingsearch.sh
 
 sh ~/.local/tmp/bingsearch.sh    
 
-sxiv ~/.local/tmp
+sxiv ~/.local/tmp/bing_images/
